f3b1182ff8e47eb061d5b047f03c73b31772564a
mtcarto <morgan.thomps@gmail.com>
2025-04-28 11:40:43 -0700

    fix NT helperfunction uninstall calls

--- helperfunctions/helperFunctionsCasfriUninstall.sql
+++ helperfunctions/helperFunctionsCasfriUninstall.sql
@@ -25,7 +25,7 @@ DROP FUNCTION IF EXISTS TT_avi01_stand_structure_validation(text, text, text, te
 DROP FUNCTION IF EXISTS TT_fvi01_stand_structure_validation(text,text,text,text,text,text,text,text,text);
 DROP FUNCTION IF EXISTS TT_vri01_hasCountOfNotNull(text, text, text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_ns_nsi01_hasCountOfNotNull(text, text, text, text, text);
-DROP FUNCTION IF EXISTS TT_fvi01_hasCountOfNotNull(text, text, text, text, text, text);
+DROP FUNCTION IF EXISTS TT_fvi01_hasCountOfNotNull(text, text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_on_fim02_hasCountOfNotNull(text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_pe_pei01_hasCountOfNotNull(text, text, text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS tt_sfv01_hasCountOfNotNull(text, text, text, text, text, text, text, text, text, text, text, text);
@@ -94,8 +94,8 @@ DROP FUNCTION IF EXISTS TT_fim_species_percent_translation(text, text);
 DROP FUNCTION IF EXISTS TT_fim02_stand_structure_translation(text, text, text);
 DROP FUNCTION IF EXISTS TT_yvi01_nat_non_veg_translation(text, text, text);
 DROP FUNCTION IF EXISTS TT_yvi01_non_for_veg_translation(text, text, text);
-DROP FUNCTION IF EXISTS TT_fvi01_countOfNotNull(text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_vri01_countOfNotNull(text, text, text, text, text, text, text, text, text);
+DROP FUNCTION IF EXISTS TT_fvi01_countOfNotNull(text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_sfv01_countOfNotNull(text, text, text, text, text, text, text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_sfv01_stand_structure_translation(text, text, text, text);
 DROP FUNCTION IF EXISTS TT_sk_utm01_species_percent_translation(text,text,text,text,text,text);
@@ -161,7 +161,7 @@ DROP FUNCTION IF EXISTS TT_yt_yvi02_stand_structure_translation(text, text, text
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_copyText(text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_copyInt(text, text, text, text, text);
 DROP FUNCTION IF EXISTS TT_yt_yvi02_disturbance_matchList(text, text, text, text, text, text);
-DROP FUNCTION IF EXISTS TT_nt_lyr_layer_translation(text, text, text, text, text, text);
+DROP FUNCTION IF EXISTS TT_nt_lyr_layer_translation(text, text, text, text, text, text, text, text);
 -------------------------------------------------------------------------------
 -- Generic functions
 -------------------------------------------------------------------------------
