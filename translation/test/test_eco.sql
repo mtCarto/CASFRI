33b6444945e0aa172148fd5f794f6f4aa54ce994
Yasmin Evans <83417667+yasmin-evans@users.noreply.github.com>
2025-08-29 13:56:00 -0700

    Update test_eco.sql
    type filter textINSERT INTO casfri50_test.eco_all_new typo fixed

--- translation/test/test_eco.sql
+++ translation/test/test_eco.sql
@@ -193,7 +193,7 @@ INSERT INTO casfri50_test.eco_all_new
 SELECT * FROM TT_Translate_ns_eco_test('rawfri', 'ns01_l1_to_ns_nsi_l1_map_3000_eco');
 ------------------------
 SELECT TT_CreateMappingView('rawfri', 'ns02', 'ns_nsi', 3000, NULL, 'eco'); -- Generates 254 ECO rows
-type filter textINSERT INTO casfri50_test.eco_all_new 
+INSERT INTO casfri50_test.eco_all_new 
 SELECT * FROM TT_Translate_ns_eco_test('rawfri', 'ns02_l1_to_ns_nsi_l1_map_3000_eco');
 ------------------------
 SELECT TT_CreateMappingView('rawfri', 'ns03', 'ns_nsi', 800, NULL, 'eco'); -- Generates 0 ECO rows
