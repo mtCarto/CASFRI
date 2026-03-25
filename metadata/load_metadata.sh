5ab549d15524ff7409cb567553b03651e8a7af09
Morgan Thompson <morgan.thomps@gmail.com>
2026-03-25 09:08:55 -0700

    add target translation table to load_metadata.sh

--- metadata/load_metadata.sh
+++ metadata/load_metadata.sh
@@ -10,9 +10,12 @@ thisScriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )
 
 echo "Loading inventory_metadata.csv table into the database..."
 "$ogrCmd" -f PostgreSQL "$gdalConnectionString" $thisScriptDir/inventory_metadata.csv $overwriteTable
+  -nln $targetTranslationFileSchema.inventory_metadata
 
 # Load layer metadata table
 echo "Loading layer_metadata.csv table into the database..."
-"$ogrCmd" -f PostgreSQL "$gdalConnectionString" $thisScriptDir/layer_metadata.csv $overwriteTable
+"$ogrCmd" -f PostgreSQL "$gdalConnectionString" $thisScriptDir/layer_metadata.csv $overwriteTable \
+  -nln $targetTranslationFileSchema.layer_metadata
+
 
 #echo "######################## End load_metadata.sh ########################################"
